9d19285e16ef16390fac11734228ba6cd9d12f61
Brnd08 <93061195+Brnd08@users.noreply.github.com>
2022-08-17 19:34:38 -0500

    The following was added: 1) method to find the link of the stream in the video page 2) method to download the stream for the current video 3) The driver was modified to add the livecat extension for debuggin purposes 4) Add the functionality to save the video for the current class using the methods above

--- src/main/java/pruebas/InvisibleChromeDriver.java
+++ src/main/java/pruebas/InvisibleChromeDriver.java
@@ -1,21 +1,66 @@
 package pruebas;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map;
 
 import static pruebas.DownloaderHelper.download_bytes;
 
 public class InvisibleChromeDriver {
 
+    public static String get_current_stream_url(WebDriver driver) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        ArrayList<Map<String, String>> entries = (ArrayList<Map<String, String>>) js.executeScript("return window.performance.getEntries();");
+        for (Map<String, String> entry1 : entries) {
+            String entry = entry1.get("name");
+            if (entry.contains(".ism") && entry.contains("/manifest(") || entry.contains(".m3u8"))
+                return entry;
+        }
+        throw new RuntimeException("COULDN'T GET THE STREAM URL");
+    }
+
+    public static void download_stream(String input_url, String output_file_path, String video_name) {
+        try {
+            FFmpeg ffmpeg = new FFmpeg("src/main/java/pruebas/ffmpeg.exe");
+            FFprobe ffprobe = new FFprobe("src/main/java/pruebas/ffprobe.exe");
+
+            FFmpegBuilder builder = new FFmpegBuilder()
+
+                    .setInput(input_url)     // Filename, or a FFmpegProbeResult
+                    .overrideOutputFiles(true) // Override the output if it exists
+
+                    .addOutput(output_file_path)   // Filename for the destination
+
+                    .setAudioChannels(1)         // Mono audio
+                    .setAudioCodec("copy")        // using the aac codec
+
+                    .setVideoCodec("copy")     // Video using x264
+                    .setAudioBitStreamFilter("aac_adtstoasc")
+                    .done();
+
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+// Run a one-pass encode
+            executor.createJob(builder).run();
+
+        } catch (Exception e) {
+            System.out.println("\nCOULDN'T DOWNLOAD THE VIDEO FOR THIS COURSE\n");
+            throw new RuntimeException(e);
+        }
+    }
 
     public static ChromeOptions create_invisible_chrome_options() {
         ChromeOptions options = new ChromeOptions();
@@ -28,6 +73,9 @@ public class InvisibleChromeDriver {
         options.addArguments("window-size=1920,1080");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
         options.addArguments("disable-infobars");
+        File livecatExtension = new File("src/main/java/pruebas/livecat.crx");
+        options.addExtensions(livecatExtension);
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         return options;
     }
 
@@ -98,7 +146,7 @@ public class InvisibleChromeDriver {
             download_all_files_button = driver.findElement(By.className("FilesTree-download"));
             link = download_all_files_button.getAttribute("href");
         } catch (Exception e) {
-            System.out.println("COULDN'T FIND FILES FOR THIS VIDEO \t" + e.getMessage());
+            System.out.println("COULDN'T FIND FILES FOR THIS VIDEO \t");
             return;
         }
         try {
@@ -116,7 +164,7 @@ public class InvisibleChromeDriver {
         WebDriver driver;
         driver = new ChromeDriver(create_invisible_chrome_options());
 
-        String url_course = "https://platzi.com/clases/3208-programacion-basica/51992-maquetacion-con-html";
+        String url_course = "https://platzi.com/clases/3208-programacion-basica/51979-crea-tu-primer-sitio-web";
         driver.get(url_course);
 
         String printFormat = "%-40s --> %-30s \n";
@@ -135,8 +183,22 @@ public class InvisibleChromeDriver {
         System.out.format(printFormat, "The total NUMBER OF VIDEOS is", total_videos_number);
 
         System.out.println("STARTING DOWNLOADING VIDEO FILES...");
-        download_current_video_files(driver, current_video_title + " - " + driver.findElement(By.className("FilesAndLinks-title")).getText());
 
+        String file_to_download_name = current_video_title + " - " + driver.findElement(By.className("FilesAndLinks-title")).getText();
+        try {
+            download_current_video_files(driver, file_to_download_name);
+        } catch (Exception e) {
+            System.out.println("COULDN'T FIND FILES FOR THIS VIDEO \t" + e.getMessage());
+        }
+
+        String streaming_url = get_current_stream_url(driver);
+        System.out.format(printFormat, "Streaming link", streaming_url);
+
+        System.out.format(printFormat, "STARTING DOWNLOADING CURRENT VIDEO", current_video_title);
+
+        String video_name = current_video_number + ".- " + current_video_title + ".mp4";
+        download_stream(streaming_url, "C:\\Users\\brdn\\OneDrive - Instituto Politecnico Nacional\\Desktop\\" + video_name, current_video_title);
+        System.out.println("THE VIDEO FOR THIS COURSE WAS SUCCESFULLY DOWNLOADED");
 
 //        driver.quit();
     }
